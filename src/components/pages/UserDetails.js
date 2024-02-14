bddd15368c46dbb5b6223023154c6eacd37780c8
arjunroy7979 <arjunroy7979@gmail.com>
2024-02-14 16:17:01 +0530

    react-redux and another features add

--- src/components/pages/UserDetails.js
+++ src/components/pages/UserDetails.js
@@ -1,52 +1,49 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { deleteUser } from '../../redux/UserReducer'
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const UserDetails = () => {
     const users = useSelector((state) => state.users)
     const dispatch = useDispatch()
-    const navigate = useNavigate();
 
     const handleDelete = (id) => {
         if (window.confirm("Are you sure you want to Delete?")) {
             dispatch(deleteUser({ id: id }))
-            navigate('/')
         }
     }
 
     return (
         <div className='container-fluid'>
-            <div className='container'>
-                <table className='table'>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Phone</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            users.map((user, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td>{user.id}</td>
-                                        <td>{user.name}</td>
-                                        <td>{user.email}</td>
-                                        <td>{user.phone}</td>
-                                        <td>
-                                            <button className='btn btn-sm btn-danger' onClick={() => handleDelete(user.id)}>delete</button>
-                                        </td>
-                                    </tr>
-                                )
-                            })
-                        }
-                    </tbody>
-                </table>
-            </div>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((user, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{user.id}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.phone}</td>
+                                    <td>
+                                        <button className='btn btn-sm btn-danger' onClick={() => handleDelete(user.id)}>delete</button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+                    <Link to={'/'} className='border px-5 rounded'>Back</Link>
+                </tbody>
+            </table>
         </div>
     )
 }
